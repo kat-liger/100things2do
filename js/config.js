e09451129738d54b714f6431a0574d4029bbe61d
Kat Liger <kat.liger@gmail.com>
2015-08-03 10:42:46 -0700

    jQuery.hammer and Waves test

--- js/config.js
+++ js/config.js
@@ -8,16 +8,20 @@ require.config({
         //Libraries
         jquery: 'libs/jquery.min',
         lodash: 'libs/lodash.min',
+        parse: 'libs/parse.min',
         json2: 'libs/json2',
         text: 'libs/text',
         hammerjs: 'libs/hammer.min',
-        parse: 'libs/parse.min',
+        'jquery.hammer': 'jquery.hammer',
+        waves: 'waves.min',
         materialize: 'libs/materialize.min'
+
     },
 
     shim: {
         parse: { deps: ['lodash', 'jquery'], exports: 'Parse'},
-        materialize: { deps: ['jquery', 'hammerjs'] }
+        materialize: { deps: ['jquery', 'hammerjs'] },
+        'jquery.hammer': { deps: ['jquery', 'hammerjs', 'waves'] }
     }
 
 });
